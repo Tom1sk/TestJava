f77877c0724b574b3feefd29eb4142412c51cee3
Tom1sk <tom.ruzicka21@gmail.com>
2023-03-28 08:47:59 +0200

    Test Java

---
+++ src/ZlomekTest.java
@@ -0,0 +1,48 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ZlomekTest {
+
+    @Test
+    void getCitatel() {
+        Zlomek zlomek = new Zlomek(3,3);
+        assertEquals(3, zlomek.getCitatel());
+    }
+
+    @Test
+    void setCitatel() {
+    }
+
+    @Test
+    void getJmenovatel() {
+    }
+
+    @Test
+    void setJmenovatel() {
+    }
+
+    @Test
+    void scitani() {
+    }
+
+    @Test
+    void odcitani() {
+    }
+
+    @Test
+    void nasobeni() {
+    }
+
+    @Test
+    void deleni() {
+    }
+
+    @Test
+    void kraceni() {
+    }
+
+    @Test
+    void NSD() {
+    }
+}
